ba072755993ee12d40c5b2851c6e48f574bc2ba4
Joey McKenzie <joey.mckenzie27@gmail.com>
2024-07-12 14:28:14 -0700

    chore: wip

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,6 @@ import { defineConfig } from 'astro/config';
 import icon from 'astro-icon';
 import sitemap from '@astrojs/sitemap';
 import robotsTxt from 'astro-robots-txt';
-
 import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,8 +2,5 @@ import type { Config } from 'tailwindcss';
 
 export default {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+  darkMode: 'class',
 } satisfies Config;
