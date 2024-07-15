2034aa5c92b2d3e72a0289f86930ebf49fbf33ac
Joey McKenzie <joey.mckenzie27@gmail.com>
2024-07-14 22:51:03 -0700

    chore: wip

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,8 +4,10 @@ import sitemap from '@astrojs/sitemap';
 import robotsTxt from 'astro-robots-txt';
 import tailwind from '@astrojs/tailwind';
 
+import alpinejs from '@astrojs/alpinejs';
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://astro-codestitch-starter.com',
-  integrations: [icon(), sitemap(), robotsTxt(), tailwind()],
+  integrations: [icon(), sitemap(), robotsTxt(), tailwind(), alpinejs()],
 });
