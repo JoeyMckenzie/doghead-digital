bdad28d94783b9bedd4128c050341a5c3f9572f2
Joey McKenzie <joey.mckenzie27@gmail.com>
2024-07-18 22:42:34 -0700

    chore: wip

--- src/scripts/tsparticles.ts
+++ src/scripts/tsparticles.ts
@@ -1,9 +1,15 @@
 import { tsParticles } from 'tsparticles-engine';
 import { loadLinksPreset } from 'tsparticles-preset-links';
+import { loadStarsPreset } from 'tsparticles-preset-stars';
 
 // @ts-expect-error added to window
-window.particlesInit = async function () {
+window.particlesInitForHero = async function () {
   await loadLinksPreset(tsParticles);
+};
+
+// @ts-expect-error added to window
+window.particlesInitForSbs = async function () {
+  await loadStarsPreset(tsParticles);
 };
 
 // @ts-expect-error added to window
